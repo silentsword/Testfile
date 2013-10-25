40bced1145abe993832fe8e2f460ef6949fca58f
silentsword <cooldmonday@gmail.com>
2013-10-25 02:17:22 -0500

    update test file

--- test4.c
+++ test4.c
@@ -14,13 +14,13 @@ int main()
 		printf("Failed to create a group.\n");
     if(IGCreate(2) == 0)
 		printf("Failed to create a group.\n");
-//the amount of the group cannot greater than 20.
+	//the amount of the group cannot be greater than 20
     for(int i =0;i<18;i++){
     	if(IGCreate(i+3) == 0)
     	printf("Failed to create a group at creating group %d ,\n",i+3);
     }
 	
-	//test IGPublisher
+	//fork processes
 	pid_t pid, pid2,pid3,pid4;
 	pid = fork();
     if (pid == -1)
@@ -30,55 +30,49 @@ int main()
 	}
     if(pid != 0){
     	pid2 = fork();
-    	if (pid2 == -1)
-    	   {
+    	if (pid2 == -1){
     		  fprintf(stderr, "can't fork, error %d\n", errno);
     	      exit(EXIT_FAILURE);
-    	   }
+    	}
     	if(pid2!=0){
     		pid3 = fork();
-    		if (pid3 == -1)
-    		   {
+    		if (pid3 == -1){
     			fprintf(stderr, "can't fork, error %d\n", errno);
     			exit(EXIT_FAILURE);
-    		   }
+    		}
     		if(pid3!=0){
     			pid4=fork();
-    			if (pid4 == -1)
-    				{
+    			if (pid4 == -1){
     			    fprintf(stderr, "can't fork, error %d\n", errno);
     			    exit(EXIT_FAILURE);
-    			    }
-    			if(pid4==0){
-    				//test IGSubscriber
+    			}
+    			if(pid4==0){ //test IGSubscriber
     				if(IGSubscriber(0) == 0)
     					printf("Failed to declare as a publisher.\n");
     				if(IGSubscriber(1) == 0)
     					printf("Failed to declare as a publisher.\n");
     			}
-    		}else{
+    		}else{ //test IGSubscriber
     			if(IGSubscriber(0) == 0)
     			    printf("Failed to declare as a publisher.\n");
     			if(IGSubscriber(1) == 0)
     			    printf("Failed to declare as a publisher.\n");
     		}
     	}
-    		else{
+    		else{ //test IGPublisher
     			if(IGPublisher(0) == 0)
     				printf("Failed to declare as a publisher.\n");
     			if(IGPublisher(1) == 0)
     				printf("Failed to declare as a publisher.\n");
     		}
-    }else{
+    }else{ //test IGPublisher
 		if(IGPublisher(0) == 0)
 			printf("Failed to declare as a publisher.\n");
+		//test IGPublish
 		IGPublish(0, "How are you?");
 		
 	}
-	
 	//test IGPublish
-	
-	
-	//test IGRetrive
-	
+	//test IGSubscribe
+	sleep(20);
 }
